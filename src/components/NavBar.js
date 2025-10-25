83bd386453b9480001b140ba942c81198ed0165f
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2025-10-26 02:13:16 +0300

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <nav>
+      <NavLink to="/" className="nav-link">
+        Home
+      </NavLink>
+      <NavLink to="/about" className="nav-link">
+        About
+      </NavLink>
+      <NavLink to="/login" className="nav-link">
+        Login
+      </NavLink>
+    </nav>
+  );
+}
+
+export default NavBar;
